67659486c59a2ad2c85e1036a311f4b4b0af4012
echoMarcoAntonio <m.douglas.enterprises@outlook.com.br>
2025-05-26 17:25:27 -0300

    Tipos de dados & Concatenação

---
+++ 1-tiposDados.py
@@ -0,0 +1,13 @@
+# Pythonflix
+
+# Dados do filme
+name = "Top Gun Maverick"
+yearLaunch = 1989
+movieRanking = 9.5
+planIncluded = False
+
+# Imprime o tipo do dado "name"
+print(type(name))
+
+# Imprime o valor de "yearLaunch"
+print(yearLaunch)
---
+++ 2-input.py
@@ -0,0 +1,13 @@
+# Utilizando o Input
+
+# Tipos de dados definidos
+name = input("Digite o nome do filme:\n")
+yearLaunch = int(input("Digite o ano de lançamento do filme:\n"))
+movieRanking = float(input("Digite a nota do filme:\n"))
+
+# Note que: input transforma as variáveis em "string". Portanto, define-se manualmente o tipo do dado.
+
+# Imprimindo variáveis
+print(type(name))
+print(type(yearLaunch))
+print(type(movieRanking))
